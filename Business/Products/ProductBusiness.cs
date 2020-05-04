bec9ad47c14fc3a7e62286f6ffa241b83fa00389
Ales Florez <ales.florez@viamericas.com>
2020-05-04 14:49:17 -0500

    Add new product business layer

---
+++ Business/Products/ProductBusiness.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Viam.SalesForce.API.Data.Products;
+using Viam.SalesForce.API.Model.Configuration;
+
+namespace Viam.SalesForce.API.Business.Products
+{
+    public class ProductBusiness
+    {
+        ProductRepository _envioReadRepository;
+
+        public ProductBusiness(ConfigurationModel configuration)
+        {
+            _envioReadRepository = new ProductRepository(configuration.EnvioRead);
+        }
+
+        public ActionResult<string> setProductStatus(string idbranch, string status, string flag)
+        {
+            return _envioReadRepository.setProductStatus(idbranch, status, flag);
+        }
+
+    }
+}
