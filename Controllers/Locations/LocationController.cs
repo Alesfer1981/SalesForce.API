e602076d46ae651f2338beddeb52bff2025f3cc4
Ales Florez <ales.florez@viamericas.com>
2020-05-04 14:55:47 -0500

    Add SetSynchronized and setLocationStatus methods

--- Controllers/Locations/LocationController.cs
+++ Controllers/Locations/LocationController.cs
@@ -21,10 +21,19 @@ namespace Viam.SalesForce.API.Controllers.Locations
     [Authorize]
     public class LocationController : Controller
     {
+        /// <summary>
+        /// Log management object
+        /// </summary>
         private readonly ILogger<LocationController> _logger;
 
+        /// <summary>
+        /// Configuration object
+        /// </summary>
         private readonly IOptions<ConfigurationModel> _configurations;
 
+        /// <summary>
+        /// Business layer object
+        /// </summary>
         private LocationBusiness _locationBusiness = null;
 
         /// <summary>
@@ -43,8 +52,8 @@ namespace Viam.SalesForce.API.Controllers.Locations
         /// Get a list of locations
         /// </summary>
         /// <param name="idLocation">Code of location</param>
-        /// <param name="synchronize"></param>
-        /// <returns></returns>
+        /// <param name="synchronize">Flag to indicate if is upated or not 1:updated 0:not updated</param>
+        /// <returns>List of locations</returns>
         [HttpGet]
         [Route("location")]
         public ActionResult<List<LocationModel>> getLocations([FromQuery] string idLocation,
@@ -71,11 +80,11 @@ namespace Viam.SalesForce.API.Controllers.Locations
         [HttpGet]
         [Route("resume")]
         public ActionResult<List<ResumeData>> getResumeData([FromQuery] string idLocation,
-                                                                        int measure)
+                                                                        string idSalesRep)
         {
             try
             {
-                return _locationBusiness.getResumeData(idLocation, measure);
+                return _locationBusiness.getResumeData(idLocation, idSalesRep);
             }
             catch (Exception ex)
             {
@@ -103,6 +112,78 @@ namespace Viam.SalesForce.API.Controllers.Locations
                 return BadRequest(ex.Message.ToString());
             }
         }
+
+        /// <summary>
+        /// Set status a group of locations
+        /// </summary>
+        /// <param name="idLocationList">List of locations codes separated by comma</param>
+        /// <param name="value">Value to set synchronize column</param>
+        /// <returns>Confirm operation</returns>
+        [HttpPut]
+        [Route("synch")]
+        public ActionResult<string> setSynchronized([FromQuery]string idLocationList, string value)
+        {
+            try
+            {
+                return _locationBusiness.setSynchronized(idLocationList, value);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"LocationController - metodo setSynchronized API synch, exception: { ex.Message} stack trace : {ex.StackTrace}");
+                return BadRequest(ex.Message.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Disable all location active products
+        /// </summary>
+        /// <param name="idLocation">Code of location</param>
+        /// <returns>Message response</returns>
+        [HttpPut]
+        [Route("locationStatus")]
+        public ActionResult<string> setLocationStatus([FromQuery]string idLocation)
+        {
+            try
+            {
+                return _locationBusiness.setLocationStatus(idLocation);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"LocationController - metodo setLocationStatus API locationStatus, exception: { ex.Message} stack trace : {ex.StackTrace}");
+                return BadRequest(ex.Message.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Get list of current rates
+        /// </summary>
+        /// <param name="idLocation">Code of location</param>
+        /// <param name="dateFrom"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("rates")]
+        public ActionResult<List<RateModel>> getCurrentRates([FromQuery] string idLocation, string dateFrom)
+        {
+            try
+            {
+                DateTime? dateProcess;
+                if (!string.IsNullOrEmpty(dateFrom))
+                {
+                    dateProcess = DateTime.Parse(dateFrom);
+                }
+                else
+                {
+                    dateProcess = null;
+                }
+                return _locationBusiness.getCurrentRates(idLocation, dateProcess);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"LocationController - metodo getCurrentRates API locationStatus, exception: { ex.Message} stack trace : {ex.StackTrace}");
+                return BadRequest(ex.Message.ToString());
+            }
+        }
+
 
         /// <summary>
         /// Test conection
