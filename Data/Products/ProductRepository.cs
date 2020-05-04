d9172e2c4fc1a30f42c38876edb535b479793d07
Ales Florez <ales.florez@viamericas.com>
2020-05-04 16:36:22 -0500

    Add setProductStatus method

--- Data/Products/ProductRepository.cs
+++ Data/Products/ProductRepository.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -25,5 +26,28 @@ namespace Viam.SalesForce.API.Data.Products
             _connectionString = connection;
         }
 
+        /// <summary>
+        /// Update product status
+        /// </summary>
+        /// <param name="idbranch">code of product</param>
+        /// <param name="flag">status value to change</param>
+        /// <returns>Message response</returns>
+        public ActionResult<string> setProductStatus(string idbranch, string status, string flag)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                dbConnection.Open();
+
+                var p = new DynamicParameters();
+                p.Add("@ID_BRANCH", idbranch);
+                p.Add("@STATUS", status);
+                p.Add("@FLAG", flag);
+
+                var query = dbConnection.Query<string>(Helper.Constants.spUpdateSalesFProductStatus,
+                                                       p,
+                                                       commandType: CommandType.StoredProcedure);
+                return query.ToString();
+            }
+        }
     }
 }
