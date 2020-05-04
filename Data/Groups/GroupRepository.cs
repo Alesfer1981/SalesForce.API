691262035f367a007deef17f9a03a27a0adf581c
Ales Florez <ales.florez@viamericas.com>
2020-05-04 16:33:16 -0500

    Add updateCredit method

--- Data/Groups/GroupRepository.cs
+++ Data/Groups/GroupRepository.cs
@@ -5,7 +5,9 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using Viam.SalesForce.API.Model.Groups;
 using Viam.SalesForce.API.Model.Locations;
 
 namespace Viam.SalesForce.API.Data.Groups
@@ -44,5 +46,37 @@ namespace Viam.SalesForce.API.Data.Groups
             }
         }
 
+        /// <summary>
+        /// Update credit of group branch
+        /// </summary>
+        /// <param name="salesFUpdateCreditParam">salesFUpdateCreditParam object</param>
+        /// <returns></returns>
+        public ActionResult<string> updateCredit(SalesFUpdateCreditParam salesFUpdateCreditParam)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                dbConnection.Open();
+
+                var p = new DynamicParameters();
+                p.Add("@ID_MAIN_BRANCH", salesFUpdateCreditParam.idMainBranch);
+                p.Add("@DEBT_LIMIT", salesFUpdateCreditParam.debtLimit);
+                p.Add("@DEBT_LIMIT_WKND", salesFUpdateCreditParam.debtLimitWknd);
+                p.Add("@ID_PHASE", salesFUpdateCreditParam.idPhase);
+                p.Add("@DEBT_LIMIT_MO", salesFUpdateCreditParam.debtLimitMo);
+                p.Add("@DEBT_LIMIT_WKND_MO", salesFUpdateCreditParam.debtLimitWkndMo);
+
+                var query = dbConnection.Query<string>(Helper.Constants.spUpdateSalesFCredit,
+                                                       p,
+                                                       commandType: CommandType.StoredProcedure);
+                if (query.Count() > 0)
+                {
+                    return HttpStatusCode.OK.ToString();
+                }
+                else
+                {
+                    return HttpStatusCode.NotFound.ToString();
+                }
+            }
+        }
     }
 }
