4cc261044a1b2ea7f07af5424895ddcede6b1781
Ales Florez <ales.florez@viamericas.com>
2020-05-04 16:39:27 -0500

    Add DTO structure to answer object

---
+++ Model/Groups/SalesFUpdateCreditParam.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Viam.SalesForce.API.Model.Groups
+{
+    public class SalesFUpdateCreditParam
+    {
+
+        public string idMainBranch { get; set; }
+
+        public decimal debtLimit { get; set; }
+
+        public decimal debtLimitWknd { get; set; }
+
+        public int idPhase { get; set; }
+
+        public decimal debtLimitMo { get; set; }
+
+        public decimal debtLimitWkndMo { get; set; }
+
+    }
+}
