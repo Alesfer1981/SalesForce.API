03d523fc42ba9a2a0bd41bd091c70ea489063d5f
Ales Florez <ales.florez@viamericas.com>
2020-06-02 12:14:22 -0500

    add swagger documentation

--- Model/Groups/SalesFUpdateCreditParam.cs
+++ Model/Groups/SalesFUpdateCreditParam.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,17 +8,35 @@ namespace Viam.SalesForce.API.Model.Groups
 {
     public class SalesFUpdateCreditParam
     {
-
+        /// <summary>
+        /// Branch Code
+        /// </summary>
+        [Required, StringLength(10), MinLength(6)]
         public string idMainBranch { get; set; }
 
+        /// <summary>
+        /// Value of debit limit branch
+        /// </summary>
         public decimal debtLimit { get; set; }
 
+        /// <summary>
+        /// Value of debit limit branch on Weekend
+        /// </summary>
         public decimal debtLimitWknd { get; set; }
 
+        /// <summary>
+        /// Code of actual branch phase
+        /// </summary>
         public int idPhase { get; set; }
 
+        /// <summary>
+        /// Value of debit limit Monthly
+        /// </summary>
         public decimal debtLimitMo { get; set; }
 
+        /// <summary>
+        /// Value of debit limit Monthly
+        /// </summary>
         public decimal debtLimitWkndMo { get; set; }
 
     }
